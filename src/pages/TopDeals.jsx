76dd4e8ca75728f9278109eea658ca4585735dc8
Rahul7740 <rahultaak7740@gmail.com>
2024-09-05 13:26:44 +0530

    asdf

--- src/pages/TopDeals.jsx
+++ src/pages/TopDeals.jsx
@@ -1,26 +1,54 @@
 import React, { Fragment, useState } from "react";
 import "../style/productFilter.css";
-import "../style/topDeal.css"
+import "../style/topDeal.css";
 import { Link } from "react-router-dom";
 import SvgPath from "../assets/svg/SvgPath";
 import colosArray from "../json/filter-color.json";
-import filterProducts from "../json/filterProducts.json"
+import topDealProducts from "../json/top-deal-products.json";
 import RelatedItemYourSearch from "../snippets/RelatedItemYourSearch";
-import ProductCard from "../snippets/ProductCard";
+import ProductCard from "../snippets/TopDealProductCard";
 import AllButtons from "../snippets/AllButtons";
 import NewsLetter from "../home/NewsLetter";
+import productCategorys from "../json/products-categorys.json";
 
 function TopDeals() {
-  const [filter, setFilter] = useState(false)
+  function heidhgn(e) {
+    let sibling = e.nextElementSibling.style
+    console.log(sibling);
+    
+    // if(sibling){
+
+    // }
+  }
+
+  const [arrow, setArrow] = useState(false);
+  function arrowRotate() {
+    setArrow(arrow === false ? true : false);
+  }
+  const [filter, setFilter] = useState(false);
+  const [categorys, setCategorys] = useState(8);
+  function categorysQuantity() {
+    setCategorys(categorys === 8 ? 11 : 8);
+  }
+  const [colors, setColor] = useState(12);
+  function colorQuantity() {
+    setColor(colors === 12 ? 24 : 12);
+  }
 
   const [value, setValue] = useState(20);
-
   const handleSliderChange = (event) => {
     setValue(event.target.value);
   };
   return (
     <>
-      <div onClick={() => { setFilter(false) }} className={`filter-background-overlay ${filter === true ? "showFIlter" : ""}`}></div>
+      <div
+        onClick={() => {
+          setFilter(false);
+        }}
+        className={`filter-background-overlay ${
+          filter === true ? "showFIlter" : ""
+        }`}
+      ></div>
 
       <section className="all-sections">
         <div className="container">
@@ -29,49 +57,87 @@ function TopDeals() {
             <div className="pages-directions-div">
               <Link to={"/"}>Home</Link>
               <img src={SvgPath.arrowLIneRight} alt="forword" />
-              <Link style={{ color: "#1F292D" }}  to={"/topDeals"}>Top Deals</Link>
+              <Link style={{ color: "#1F292D" }} to={"/topDeals"}>
+                Top Deals
+              </Link>
             </div>
           </div>
           <div className="filter_and_viewAll-container">
-            <div className={`filter-container ${filter === true ? "showFIlter" : ""}`}>
+            <div
+              className={`filter-container ${
+                filter === true ? "showFIlter" : ""
+              }`}
+            >
               <div className="filter-head">
                 <h2>Filter</h2>
                 <button className="display-block-none-1100">CLEAR ALL</button>
-                <button onClick={() => { setFilter(false) }} className={`display-none-block-1100 }`}><img src={SvgPath.closeBtn} alt="CLOSE" /></button>
+                <button
+                  onClick={() => {
+                    setFilter(false);
+                  }}
+                  className={`display-none-block-1100 }`}
+                >
+                  <img src={SvgPath.closeBtn} alt="CLOSE" />
+                </button>
               </div>
-              <details className="filter-category" open>
-                <summary className="filter-summary">
+              <div className="top-deal-caterogy" open>
+                <div
+                  onClick={(e) => {
+                    arrowRotate();
+                    heidhgn(e);
+                  }}
+                  className="top-deal-caterogy-content"
+                >
                   <h3 className="filter-headings">Top Deals</h3>
-                  <img className="down-svg" src={SvgPath.upArrow} alt="upArrow" />
-                </summary>
-                <div className="filter-category-div">
+                  <img
+                    style={{
+                      transform: arrow === false ? "rotate(180deg)" : "",
+                    }}
+                    className="down-svg"
+                    src={SvgPath.upArrow}
+                    alt="upArrow"
+                  />
+                </div>
+                <div
+                  className={`topDeal-category-div ${
+                    arrow === true ? "heidfdf" : ""
+                  }`}
+                >
                   <button>All Deals</button>
                   <button>Today Deals</button>
                   <button>Upcoming Deals</button>
                   <button>Available Products Only</button>
                 </div>
-              </details>
+              </div>
               <details className="filter-category" open>
                 <summary className="filter-summary">
                   <h3 className="filter-headings">Category</h3>
-                  <img className="down-svg" src={SvgPath.upArrow} alt="upArrow" />
+                  <img
+                    className="down-svg"
+                    src={SvgPath.upArrow}
+                    alt="upArrow"
+                  />
                 </summary>
                 <div className="filter-category-div">
-                  <button>Watches</button>
-                  <button>Mobiles</button>
-                  <button>Laptops</button>
-                  <button>Tablets</button>
-                  <button>Headphones</button>
-                  <button>Printers</button>
-                  <button>Scanners</button>
-                  <button>Speakers</button>
+                  {productCategorys.slice(0, categorys).map((item, index) => (
+                    <button key={index}>{item.name}</button>
+                  ))}
                 </div>
-                <button className="filter-viewALl-btn">View all</button>
+                <button
+                  onClick={categorysQuantity}
+                  className="filter-viewALl-btn"
+                >
+                  {categorys === 8 ? "View all" : "Show Less"}
+                </button>
               </details>
               <details className="filter-category" open>
                 <summary className="filter-summary">
                   <h3 className="filter-headings">Price</h3>
-                  <img className="down-svg" src={SvgPath.upArrow} alt="upArrow" />
+                  <img
+                    className="down-svg"
+                    src={SvgPath.upArrow}
+                    alt="upArrow"
+                  />
                 </summary>
 
                 <div className="filter-price-range">
@@ -83,7 +149,10 @@ function TopDeals() {
                     className="slider"
                     onChange={handleSliderChange}
                   />
-                  <div className="tooltip" style={{ left: `calc(${(value - 50) / 4.9}% - 10px)` }}>
+                  <div
+                    className="tooltip"
+                    style={{ left: `calc(${(value - 50) / 4.9}% - 10px)` }}
+                  >
                     {value}
                   </div>
                 </div>
@@ -95,38 +164,67 @@ function TopDeals() {
               <details className="filter-category" open>
                 <summary className="filter-summary">
                   <h3 className="filter-headings">Color</h3>
-                  <img className="down-svg" src={SvgPath.upArrow} alt="upArrow" />
+                  <img
+                    className="down-svg"
+                    src={SvgPath.upArrow}
+                    alt="upArrow"
+                  />
                 </summary>
                 <div className="category-div">
                   <form className="filter-form">
-                    {colosArray.map((i, index) => (
+                    {colosArray.slice(0, colors).map((i, index) => (
                       <Fragment key={index}>
-                        <input className="filter-color-input" id={i.id} type="radio" name="asdf" />
+                        <input
+                          className="filter-color-input"
+                          id={i.id}
+                          type="radio"
+                          name="asdf"
+                        />
                         <label className="filter-color-label" htmlFor={i.id}>
-                          <div style={{ background: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})` }} className="colors color1"></div>
+                          <div
+                            style={{ background: i.color }}
+                            className="colors color1"
+                          ></div>
                         </label>
                       </Fragment>
                     ))}
                   </form>
-                  <button className="filter-viewALl-btn">+12 more</button>
+                  <button
+                    onClick={colorQuantity}
+                    className="filter-viewALl-btn"
+                  >
+                    {colors === 12 ? "+12 more" : "Show Less"}
+                  </button>
                 </div>
               </details>
               <details className="filter-category" open>
                 <summary className="filter-summary">
                   <h3 className="filter-headings">Brands</h3>
-                  <img className="down-svg" src={SvgPath.upArrow} alt="upArrow" />
+                  <img
+                    className="down-svg"
+                    src={SvgPath.upArrow}
+                    alt="upArrow"
+                  />
                 </summary>
               </details>
               <details className="filter-category" open>
                 <summary className="filter-summary">
                   <h3 className="filter-headings">Customer review</h3>
-                  <img className="down-svg" src={SvgPath.upArrow} alt="upArrow" />
+                  <img
+                    className="down-svg"
+                    src={SvgPath.upArrow}
+                    alt="upArrow"
+                  />
                 </summary>
               </details>
               <details className="filter-category" open>
                 <summary className="filter-summary">
                   <h3 className="filter-headings">Discount</h3>
-                  <img className="down-svg" src={SvgPath.upArrow} alt="upArrow" />
+                  <img
+                    className="down-svg"
+                    src={SvgPath.upArrow}
+                    alt="upArrow"
+                  />
                 </summary>
               </details>
             </div>
@@ -135,19 +233,37 @@ function TopDeals() {
               <div className="products-view-all-head">
                 <p>Showing 1–9 of 200 results</p>
                 <div className="filter-btns-container">
-                  <button onClick={() => { setFilter(true) }} className="filter-btn">FILTER <img src={SvgPath.downArrowperpal} alt="down-arrow" /></button>
-                  <button className="sortBy-btn">SORT BY <img src={SvgPath.downArrowperpal} alt="down-arrow" /></button>
+                  <button
+                    onClick={() => {
+                      setFilter(true);
+                    }}
+                    className="filter-btn"
+                  >
+                    FILTER{" "}
+                    <img src={SvgPath.downArrowperpal} alt="down-arrow" />
+                  </button>
+                  <button className="sortBy-btn">
+                    SORT BY{" "}
+                    <img src={SvgPath.downArrowperpal} alt="down-arrow" />
+                  </button>
                 </div>
-
               </div>
               <div className="topDeal-top-Products-container">
                 <div className="topDeal-top-Products-1">
                   <div>
-                    <h3>Jabra (Wireless Headsets) <span style={{ color: "#422659" }}>20% off</span></h3>
+                    <h3>
+                      Jabra (Wireless Headsets){" "}
+                      <span style={{ color: "#422659", fontWeight: "400" }}>
+                        20% off
+                      </span>
+                    </h3>
                     <p>$ 600.00</p>
-                    <AllButtons name="BUY NOW" />
+                    <AllButtons name="BUY NOW" class="top-deal-but-btn" />
                   </div>
-                  <img src={require('../assets/images/topDeal-img-1.png')} alt="top" />
+                  <img
+                    src={require("../assets/images/topDeal-img-1.png")}
+                    alt="top"
+                  />
                 </div>
                 <div className=" topProduct-2 topDeal-top-Products-1">
                   <div>
@@ -155,29 +271,35 @@ function TopDeals() {
                     <h6>$ 600.00</h6>
                     <p>Jabra (Wireless Headsets)</p>
                   </div>
-                  <img src={require('../assets/images/topDeal-img-2.png')} alt="top" />
+                  <img
+                    src={require("../assets/images/topDeal-img-2.png")}
+                    alt="top"
+                  />
                 </div>
               </div>
               <div className="view-all-products-grid">
-                {filterProducts.map((i, index) => (
+                {topDealProducts.map((i, index) => (
                   <Fragment key={index}>
                     <ProductCard
                       img={i.img}
                       name={i.name}
                       price={i.price}
-                      ratting={i.ratting}
-                      new={i.new}
+                      discount={i.discount}
                     />
                   </Fragment>
                 ))}
               </div>
               <div className="products-pages-controler">
-                <button><img src={SvgPath.leftVictor} alt="back" /></button>
+                <button>
+                  <img src={SvgPath.leftVictor} alt="back" />
+                </button>
                 <button className="page-select">1</button>
                 <button>2</button>
                 <img src={SvgPath.threeDOts} alt="dots" />
                 <button>10</button>
-                <button><img src={SvgPath.rightVictor} alt="next" /></button>
+                <button>
+                  <img src={SvgPath.rightVictor} alt="next" />
+                </button>
               </div>
             </div>
           </div>
---
+++ src/snippets/TopDealProductCard.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/topDeal-product-card.css";
+import colorsDots from "../json/product-card-color-dots.json"
+import SvgPath from "../assets/svg/SvgPath";
+
+function TopDealProductCard(props) {
+  return (
+    <>
+      <Link to={"/productDetail"}>
+        <div className="topDeal-product-card">
+            <div className="topDeak-prodcut-card-discount">{props.discount}</div>
+          <div className="topDeal-product-card-img">
+            <img
+              src={require(`../assets/images/${props.img}`)}
+              alt="product-card"
+            />
+          </div>
+          <div className="prodcut-card-colors-dots">
+            {
+                colorsDots.map((i,index)=>(
+                    <span key={index} style={{background:i.colors}}></span>
+                ))
+            }
+          </div>
+          <h3 className="topDeal-product-card-heading">{props.name}</h3>
+          <div className="topDeal-product-card-price-Buy">
+            <p>{props.price}</p>
+            <button className="prodcut-card-2-buyBtn">
+              BUY
+              <img
+                src={require("../assets/svg/right-victor-perpul.svg").default}
+                alt="right victor"
+              />
+            </button>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+}
+
+export default TopDealProductCard;
